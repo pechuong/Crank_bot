2b36f55bd095a0920de057361ce84b3f07b34d14
pechuong <pc9673@gmail.com>
2019-11-18 02:37:21 -0800

    Created Discord Bot + 1 command

---
+++ src/main/java/Crank_Bot/commands/StaticFieldCommand.java
@@ -0,0 +1,25 @@
+package Crank_Bot.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+public class StaticFieldCommand extends Command {
+	
+	public StaticFieldCommand() {
+		this.name = "static-field";
+		this.aliases = new String[]{"silence"};
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		Guild guild = event.getGuild();
+		VoiceChannel target = event.getMember().getVoiceState().getChannel();
+		AudioManager manager = guild.getAudioManager();
+		
+	}
+
+}
