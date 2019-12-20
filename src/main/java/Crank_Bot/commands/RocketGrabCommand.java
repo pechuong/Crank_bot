4e9a613934d684405fbdbb69d055f68a7dd6c6d3
Peter <peter@PC.local>
2019-12-20 03:11:19 -0800

    Implemented specific people pulling for rocketgrab cmd

--- src/main/java/Crank_Bot/commands/RocketGrabCommand.java
+++ src/main/java/Crank_Bot/commands/RocketGrabCommand.java
@@ -1,7 +1,6 @@
 package Crank_Bot.commands;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.jagrosh.jdautilities.command.Command;
@@ -9,6 +8,8 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
 /**
@@ -26,45 +27,35 @@ public class RocketGrabCommand extends Command {
 	}
 	
 	@Override
-	protected void execute(CommandEvent event) {
-		
-		String[] args = event.getArgs().length() > 0 ? event.getArgs().split("\\s") : null;
-		
+	protected void execute(CommandEvent event) {		
 		/* Makes sure that a member is in voice channel to use this */
 		if (!event.getMember().getVoiceState().inVoiceChannel()) {
 			event.reply("YOu NeEd tO bE in a vOiCe ChanNel to uSe This!");
 			return;
 		}
 		
+		String[] args = event.getArgs().length() > 0 ? event.getArgs().split("\\s") : null;
 		
 		/* Get voice channel to drag everyone to */
 		VoiceChannel location = event.getMember().getVoiceState().getChannel();
 		Guild guild = event.getGuild();
 		Member author = event.getMember();
 
-		/* args length will still be 1 even w/ no arg so check for it */
-		if (args != null) {
-			/* Checks for real no arg */
-			if (args[0].equalsIgnoreCase("")) {
-				event.reply("okAY " + event.getAuthor().getAsMention() + " i WilL GRab tHem AlL!");
-				event.getChannel().sendTyping().queue();
-				event.reply("ComMenCiNg rOckeT-grAb On eveRYoNe...");
-				pull(event, location, guild);
-				return;
-			} 
-			/* TODO: move outside if statement 
-			 * 	- There is also a bug here when getting member by tag
-			 * 
-			 **/
-			else if (!guild.getMemberByTag(args[0]).getVoiceState().inVoiceChannel()) {
-				event.reply("User isn't online.... unable to grab :-(");
-				return;
-			}
-			//event.reply("GEt ovER hEre");
-			//guild.moveVoiceMember(guild.getMemberById(args[1]), location).queue();
-		} else {
+		/* User wants to pull everyone */
+		if (args == null) {
+			event.getChannel().sendTyping().queue();
+			event.reply("okAY " + event.getAuthor().getAsMention() + " i WilL GRab tHem AlL!");
+			event.reply("ComMenCiNg rOckeT-grAb On eveRYoNe...");
 			pull(event, location, guild);
+			return;
 		}
+		
+		List<User> mentions = event.getMessage().getMentionedUsers();
+		for (User mentioned : mentions) {
+			Member member = guild.getMember(mentioned);
+			guild.moveVoiceMember(member, location).queue();
+		}
+		event.reply("Moving has finished....");
 	}
 	
 	private void pull(CommandEvent event, VoiceChannel location, Guild guild) {
