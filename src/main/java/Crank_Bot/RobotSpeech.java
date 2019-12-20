dfc1254d031be09ab67c1b5d4f67747be2a9d768
Peter <peter@PC.local>
2019-12-20 03:25:06 -0800

    Cleaned up some spaghetti code and testing speech randomizer

---
+++ src/main/java/Crank_Bot/RobotSpeech.java
@@ -0,0 +1,15 @@
+package Crank_Bot;
+
+class RobotSpeech {
+	public static String robotify(String message) {
+		StringBuilder converted = new StringBuilder();
+		for (char c : message.toCharArray()) {
+			if (Math.random() >= 0.25) {
+				converted.append(c);
+			} else {
+				converted.append(Character.toUpperCase(c));
+			}
+		}
+		return converted.toString();
+	}
+}
