c4f5e2fe6d32160b6dab3d2844f518930ac4fc15
Peter <peter@PC.local>
2020-02-24 13:10:15 -0800

    Test command to try and spot invisible users

---
+++ src/main/java/Crank_Bot/commands/RedTrinketCommand.java
@@ -0,0 +1,29 @@
+package Crank_Bot.commands;
+
+import java.util.List;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import net.dv8tion.jda.api.entities.ClientType;
+import net.dv8tion.jda.api.entities.Member;
+
+public class RedTrinketCommand extends Command {
+	
+	public RedTrinketCommand() {
+		this.name = "red-trinket";
+		this.aliases = new String[]{"pink"};
+		this.help = "Tells if the user/users is invis or not";
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		// TODO Auto-generated method stub
+		List<Member> members = event.getMessage().getMentionedMembers();
+		for (Member m : members) {
+			m.getUser().openPrivateChannel();
+			event.reply(m.getOnlineStatus(ClientType.DESKTOP).name().equalsIgnoreCase("INVISIBLE") ? "invis" : "no");
+		}
+	}
+
+}
